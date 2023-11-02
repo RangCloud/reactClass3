590c93a6c3e7b67a308a3efd0f6a974b2ac2d786
RangCloud <mtr008@naver.com>
2023-11-02 09:49:31 +0900

    231102

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import styled from 'styled-components';
 
 
-const SimpleButton = styled.button=`
+const SimpleButton = styled.button`
   color: white;
   background-color: green;
   `;
 
+const LargeButton = styled(SimpleButton)`
+  font-size:50px
+  `;
+
+const ReactButton = props=>{
+  return <button>{props.children}</button>
+}
+
 function App() {
   return (
     <div>
-      <SimpleButton>simple</SimpleButton>
+      <SimpleButton>Simple</SimpleButton>
+      <LargeButton>Large</LargeButton>
+      <ReactButton>react</ReactButton>
     </div>
   );
 }
